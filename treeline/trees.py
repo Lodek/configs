02c4ee301790eae5cfcd8698640664c62af5b460
Lodek <23224552+Lodek@users.noreply.github.com>
2018-02-28 14:13:31 -0300

    added few zsh functions

--- treeline/trees.py
+++ treeline/trees.py
@@ -1,7 +1,9 @@
 import os
 home = os.getenv('HOME')
 trees = {}
-trees['usb'] = home+'/pbj/keyboard/usb-research'
+trees['usb'] = home+'/pbj/keyboard/usb'
+trees['blue'] = home+'/pbj/keyboard/bluetooth'
 trees['sand']='/home/lodek/sandbox/sand'
 trees['emacs']='/home/lodek/studies/emacs'
 trees['med']='/home/lodek/studies/meditation'
+trees['lp']='/home/lodek/physics/learning'
