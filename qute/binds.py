60896048348a2e00c5e48e5abab3863c977ee533
Bruno Gomes <23224552+Lodek@users.noreply.github.com>
2021-02-14 20:48:52 -0300

    rip qute

--- qute/binds.py
+++
@@ -1,9 +0,0 @@
-config.bind('pp', 'open -t -- {clipboard}')
-config.bind('Pp', 'open -- {clipboard}')
-config.bind('<Ctrl-d>', 'tab-close')
-config.bind('<d>', 'scroll-page 0 0.6')
-config.bind('<u>', 'scroll-page 0 -0.6')
-config.bind('<Ctrl-u>', 'undo')
-config.bind('<Shift-f>', 'hint all tab-bg')
-
-c.aliases['l'] = 'session-load'
--- qute/config.py
+++
@@ -1,8 +0,0 @@
-config.source('binds.py')
-
-c.backend = 'webengine'
-c.completion.cmd_history_max_items = -1
-c.content.cache.appcache = True
-c.scrolling.smooth = False
-c.search.ignore_case = 'smart'
-
